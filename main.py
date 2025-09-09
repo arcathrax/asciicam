6f35b916b2b02b1f0f2c9ddd150215c78f0b80dd
arcathrax <arcathrax@proton.me>
2025-09-09 19:45:17 +0200

    feat: add copy to clipboard functionality

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 from textual.app import App, ComposeResult
-from textual.widgets import Footer, Header, Label
+from textual.widgets import Footer, Header, Label, Button
 from textual.containers import VerticalScroll
+import pyperclip
 
 from camera import Camera
+
 
 class AsciiCamApp(App):
     camera = Camera()
 
     BINDINGS = [
-        ("q", "quit", "Quit application")
+        ("q", "quit", "Quit application"),
     ]
     CSS_PATH = "asciicamera.tcss"
 
@@ -21,7 +23,7 @@ class AsciiCamApp(App):
 
     async def on_mount(self) -> None:
         """Called when the app starts. Sets up periodic updates."""
-        self.set_interval(1/30, self.update_frame)
+        self.set_interval(1 / 30, self.update_frame)
         self.title = "ASCIICAM"
         self.sub_title = "A camera feed in your terminal."
 
@@ -30,14 +32,24 @@ class AsciiCamApp(App):
         yield Header()
         yield Footer()
         yield VerticalScroll(
-            self.items["previewlabel"]
+            self.items["previewlabel"],
+            Button("Copy ASCII", id="copybtn"),
         )
 
     async def update_frame(self) -> None:
         """Periodically called to update camera frames."""
-        ascii = self.camera.get_ascii_camera()
-        self.items["previewlabel"].update(ascii)
+        ascii_frame = self.camera.get_ascii_camera()
+        self.items["previewlabel"].update(ascii_frame)
+
+    async def on_button_pressed(self, event: Button.Pressed) -> None:
+        """Handle button presses."""
+        if event.button.id == "copybtn":
+            ascii_frame = self.camera.get_ascii_camera()
+            pyperclip.copy(str(ascii_frame))
+            self.sub_title = "Copied to clipboard!"
+
 
 if __name__ == "__main__":
     app = AsciiCamApp()
     app.run()
+
