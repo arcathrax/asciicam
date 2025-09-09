2797f5779bb86bc398e4c86fe5a49655e3b649e1
arcathrax <arcathrax@proton.me>
2025-09-09 18:32:36 +0200

    feat: add simple textual tui

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
-def main():
-    print("Hello from asciicam!")
+from textual.app import App, ComposeResult
+from textual.widgets import Footer, Header
+from textual.containers import VerticalScroll
 
+class AsciiCamApp(App):
+    BINDINGS = [
+        ("q", "quit", "Quit application")
+    ]
+
+    def compose(self) -> ComposeResult:
+        """Called to add widgets to the app."""
+        yield Header()
+        yield Footer()
+        yield VerticalScroll(
+        )
 
 if __name__ == "__main__":
-    main()
+    app = AsciiCamApp()
+    app.run()
